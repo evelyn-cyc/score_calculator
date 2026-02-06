82807407d4ee51a92d84fa001300bb2d80a0bd8f
Evelyn C. <evelyn.cyc@proton.me>
2026-02-06 13:44:28 +1100

    Add CSS in the footer: let them in the center

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
-let hero = document.querySelector(".hero");
-let slider = document.querySelector(".slider");
-let animation = document.querySelector("section.animation-wrapper");
+// let hero = document.querySelector(".hero");
+// let slider = document.querySelector(".slider");
+// let animation = document.querySelector("section.animation-wrapper");
 
-const time_line = new TimelineMax();
+// const time_line = new TimelineMax();
 
-// (Manipulating object,
-//  duration,
-//  manipulating object initial state,
-//  manipulating object final state,
-//  start the animation before a duration beforehands)
-time_line
-  .fromTo(hero, 1, { height: "0%" }, { height: "100%", ease: Power2.easeInOut })
-  .fromTo(
-    hero,
-    1.2,
-    { width: "80%" },
-    { width: "100%", ease: Power2.easeInOut },
-  )
-  .fromTo(
-    slider,
-    1,
-    { x: "-100%" },
-    { x: "0%", ease: Power2.easeInOut },
-    "-=1.2",
-  )
-  .fromTo(animation, 0.3, { opacity: 1 }, { opacity: 0 });
+// // (Manipulating object,
+// //  duration,
+// //  manipulating object initial state,
+// //  manipulating object final state,
+// //  start the animation before a duration beforehands)
+// time_line
+//   .fromTo(hero, 1, { height: "0%" }, { height: "100%", ease: Power2.easeInOut })
+//   .fromTo(
+//     hero,
+//     1.2,
+//     { width: "80%" },
+//     { width: "100%", ease: Power2.easeInOut },
+//   )
+//   .fromTo(
+//     slider,
+//     1,
+//     { x: "-100%" },
+//     { x: "0%", ease: Power2.easeInOut },
+//     "-=1.2",
+//   )
+//   .fromTo(animation, 0.3, { opacity: 1 }, { opacity: 0 });
 
-setTimeout(() => {
-  animation.style.pointerEvents = "none";
-}, 2500);
+// setTimeout(() => {
+//   animation.style.pointerEvents = "none";
+// }, 2500);
+
+window.addEventListener("keypress", (e) => {
+  console.log(e);
+});

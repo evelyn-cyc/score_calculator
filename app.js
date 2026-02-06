9b5b6f2c9177e848e64beb42479d949d89617daf
Evelyn C. <evelyn.cyc@proton.me>
2026-02-06 12:46:27 +1100

    Setting image TimelineMax animation: height from 0 to 100%, width from 80% to 100%

---
+++ app.js
@@ -0,0 +1,15 @@
+let hero = document.querySelector(".hero");
+let slider = document.querySelector(".slider");
+let animation = document.querySelector("section.animation-wrapper");
+
+const time_line = new TimelineMax();
+
+// (Manipulating object, duration, manipulating object initial state, manipulating object final state)
+time_line
+  .fromTo(hero, 1, { height: "0%" }, { height: "100%", ease: Power2.easeInOut })
+  .fromTo(
+    hero,
+    1.2,
+    { width: "80%" },
+    { width: "100%", ease: Power2.easeInOut },
+  );

64e72175c08df250506b688a44358b71d3458cdc
Evelyn C. <evelyn.cyc@proton.me>
2026-02-06 12:53:35 +1100

    Modification: start the slider 1.2s beforehand

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ let animation = document.querySelector("section.animation-wrapper");
 
 const time_line = new TimelineMax();
 
-// (Manipulating object, duration, manipulating object initial state, manipulating object final state)
+// (Manipulating object,
+//  duration,
+//  manipulating object initial state,
+//  manipulating object final state,
+//  start the animation before a duration beforehands)
 time_line
   .fromTo(hero, 1, { height: "0%" }, { height: "100%", ease: Power2.easeInOut })
   .fromTo(
@@ -13,4 +17,10 @@ time_line
     { width: "80%" },
     { width: "100%", ease: Power2.easeInOut },
   )
-  .fromTo(slider, 1, { x: "-100%" }, { x: "0%", ease: Power2.easeInOut });
+  .fromTo(
+    slider,
+    1,
+    { x: "-100%" },
+    { x: "0%", ease: Power2.easeInOut },
+    "-=1.2",
+  );

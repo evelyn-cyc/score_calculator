d750015219083ff0c5aeab55eb47c63f84c00308
Evelyn C. <evelyn.cyc@proton.me>
2026-02-16 09:34:26 +1100

    Remove the button default function, Change the background color while choosing different grades

--- app.js
+++ app.js
@@ -30,6 +30,64 @@
 //   animation.style.pointerEvents = "none";
 // }, 2500);
 
+//------------------------------------------------------------------
+
+// Avoid the Enter key fresh the page
 window.addEventListener("keypress", (e) => {
-  console.log(e);
+  if (e.key === "Enter") {
+    e.preventDefault();
+  }
 });
+
+// Avoid all buttons submit the form after clicking
+let allButton = document.querySelectorAll("button");
+allButton.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+});
+
+// After choosing Option in Select, change into the corresponding colors
+let allSelect = document.querySelectorAll("select");
+allSelect.forEach((select) => {
+  select.addEventListener("change", (e) => {
+    changeColor(e.target); // e.target is <select>
+  });
+});
+
+function changeColor(target) {
+  if (target.value === "A+" || target.value === "A" || target.value === "A-") {
+    target.style.backgroundColor = "lightgreen";
+    target.style.color = "black";
+  } else if (
+    target.value === "B+" ||
+    target.value === "B" ||
+    target.value === "B-"
+  ) {
+    target.style.backgroundColor = "yellow";
+    target.style.color = "black";
+  } else if (
+    target.value === "C+" ||
+    target.value === "C" ||
+    target.value === "C-"
+  ) {
+    target.style.backgroundColor = "orange";
+    target.style.color = "black";
+  } else if (
+    target.value === "D+" ||
+    target.value === "D" ||
+    target.value === "D-"
+  ) {
+    target.style.backgroundColor = "red";
+    target.style.color = "black";
+  } else if (
+    target.value === "E+" ||
+    target.value === "E" ||
+    target.value === "E-"
+  ) {
+    target.style.backgroundColor = "grey";
+    target.style.color = "black";
+  } else {
+    target.style.backgroundColor = "white";
+  }
+}
